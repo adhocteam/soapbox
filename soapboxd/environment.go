76aca774f8e89d5f3a24b4c6490e6a3e8fd47939
Paul Smith <paul@adhocteam.us>
2017-08-04 10:38:51 -0500

    Fix a few missed timestamp conversions

--- soapboxd/environment.go
+++ soapboxd/environment.go
@@ -51,7 +51,10 @@ func (s *server) ListEnvironments(ctx context.Context, req *pb.ListEnvironmentRe
 
 func (s *server) GetEnvironment(ctx context.Context, req *pb.GetEnvironmentRequest) (*pb.Environment, error) {
 	getSQL := "SELECT id, application_id, name, slug, vars, created_at FROM environments WHERE id = $1"
-	var env pb.Environment
+	env := &pb.Environment{
+		CreatedAt: new(gpb.Timestamp),
+	}
+	var createdAt time.Time
 	var vars []byte
 	dest := []interface{}{
 		&env.Id,
@@ -59,15 +62,16 @@ func (s *server) GetEnvironment(ctx context.Context, req *pb.GetEnvironmentReque
 		&env.Name,
 		&env.Slug,
 		&vars,
-		&env.CreatedAt,
+		&createdAt,
 	}
 	if err := s.db.QueryRow(getSQL, req.GetId()).Scan(dest...); err != nil {
 		return nil, errors.Wrap(err, "scanning db row")
 	}
+	setPbTimestamp(env.CreatedAt, createdAt)
 	if err := json.Unmarshal(vars, &env.Vars); err != nil {
 		return nil, errors.Wrap(err, "unmarshalling env vars JSON")
 	}
-	return &env, nil
+	return env, nil
 }
 
 func (s *server) CreateEnvironment(ctx context.Context, req *pb.Environment) (*pb.Environment, error) {
@@ -87,12 +91,14 @@ func (s *server) CreateEnvironment(ctx context.Context, req *pb.Environment) (*p
 	}
 
 	var id int
+	var createdAt time.Time
 
-	if err := s.db.QueryRow(insertSQL, args...).Scan(&id, &req.CreatedAt); err != nil {
+	if err := s.db.QueryRow(insertSQL, args...).Scan(&id, &createdAt); err != nil {
 		return nil, errors.Wrap(err, "inserting in to db")
 	}
 
 	req.Id = int32(id)
+	setPbTimestamp(req.CreatedAt, createdAt)
 
 	return req, nil
 }

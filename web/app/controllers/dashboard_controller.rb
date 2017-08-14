2eacb3429a15d12220c5ea7a52aaae28997bd30d
Bill Mill <bill@billmill.org>
2017-08-14 16:13:30 -0400

    List number of successful deployments on dashboard
    Closes #116 
    Closes #122

--- web/app/controllers/dashboard_controller.rb
+++ web/app/controllers/dashboard_controller.rb
@@ -4,5 +4,12 @@ class DashboardController < ApplicationController
   def index
     res = $api_client.list_applications(Soapbox::Empty.new)
     @num_applications = res.applications.count
+    @num_deployments = res.applications.map(&:id).map do |app_id|
+      req = Soapbox::ListDeploymentRequest.new(application_id: app_id)
+      $api_deployment_client.list_deployments(req)
+        .deployments
+        .select { |dep| dep.state == "success" }
+        .length
+    end.sum
   end
 end

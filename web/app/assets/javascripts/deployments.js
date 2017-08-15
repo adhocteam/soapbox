358c9e2dfc5820932b8aab4e7bb09ca30c6233e3
Paul Smith <paul@adhocteam.us>
2017-08-15 09:24:51 -0400

    Show latest deploy in more places

--- web/app/assets/javascripts/deployments.js
+++ web/app/assets/javascripts/deployments.js
@@ -1,63 +1,64 @@
 (function(window) {
-    "use strict";
+  "use strict";
 
-    // this component requires that
-    // the element passed in have two data attributes
-    // data-id and data-state. Also, the element needs to have
-    // a child span to display the deployment state.
-    // upon hitting the success state, this app will
-    // set the data-state attribute
-    class DeploymentsApp {
-      constructor(element, appId) {
-        this.appId = appId;
-        this.element = element;
-        this.poll = this.poll.bind(this);
-      }
-
-      start() {
-        this.poll(); // initial request
-        this.poller = setInterval(this.poll, 1000);
-      }
-
-      stop() {
-        clearInterval(this.poller);
-      }
-
-      poll() {
-        const element = this.element;
-        if (!element) return;
-
-        const appId = this.appId;
-        const { state, id } = element.dataset;
-
-        if (['success', 'failed'].includes(state)) {
-          return this.stop();
-        }
-        const req = new XMLHttpRequest();
-
-        req.onreadystatechange = () => {
-          if (req.readyState == XMLHttpRequest.DONE ) {
-            if (req.status == 200) {
-              const deploymentStatus = req.responseText;
-              if (deploymentStatus === 'success') {
-                element.classList.add('latest');
-                element.classList.remove('active');
-              }
-              if (deploymentStatus === 'failed') {
-                element.classList.add('complete');
-              }
-              element.dataset.state = req.responseText;
-              element.getElementsByTagName("span")[0].innerHTML = req.responseText;
-            } else {
-              console.log('error occurred while polling');
-            }
-          }
-        };
-
-        req.open("GET", `/applications/${appId}/deployments/${id}.json`, true);
-        req.send();
-      }
+  // this component requires that
+  // the element passed in have two data attributes
+  // data-id and data-state. Also, the element needs to have
+  // a child span to display the deployment state.
+  // upon hitting the success state, this app will
+  // set the data-state attribute
+  class DeploymentsApp {
+    constructor(element, appId) {
+      this.appId = appId;
+      this.element = element;
+      this.poll = this.poll.bind(this);
     }
+
+    start() {
+      this.poll(); // initial request
+      this.poller = setInterval(this.poll, 1000);
+    }
+
+    stop() {
+      clearInterval(this.poller);
+    }
+
+    poll() {
+      const element = this.element;
+      if (!element) return;
+
+      const appId = this.appId;
+      const { state, id } = element.dataset;
+
+      if (["success", "failed"].includes(state)) {
+        return this.stop();
+      }
+      const req = new XMLHttpRequest();
+
+      req.onreadystatechange = () => {
+        if (req.readyState == XMLHttpRequest.DONE) {
+          if (req.status == 200) {
+            const deploymentStatus = req.responseText;
+            if (deploymentStatus === "success") {
+              element.classList.add("latest");
+              element.classList.remove("active");
+            }
+            if (deploymentStatus === "failed") {
+              element.classList.add("complete");
+            }
+            element.dataset.state = req.responseText;
+            //            element.getElementsByTagName("span")[0].innerHTML =
+            //              req.responseText;
+          } else {
+            console.log("error occurred while polling");
+          }
+        }
+      };
+
+      req.open("GET", `/applications/${appId}/deployments/${id}.json`, true);
+      req.send();
+    }
+  }
 
   window.DeploymentsApp = DeploymentsApp;
 })(window);
--- web/app/controllers/dashboard_controller.rb
+++ web/app/controllers/dashboard_controller.rb
@@ -3,13 +3,33 @@ require 'application_pb'
 class DashboardController < ApplicationController
   def index
     res = $api_client.list_applications(Soapbox::Empty.new)
-    @num_applications = res.applications.count
-    @num_deployments = res.applications.map(&:id).map do |app_id|
-      req = Soapbox::ListDeploymentRequest.new(application_id: app_id)
-      $api_deployment_client.list_deployments(req)
-        .deployments
-        .select { |dep| dep.state == "success" }
-        .length
-    end.sum
+    apps = res.applications
+    @num_applications = apps.count
+    @num_deployments = 0
+    apps.each do |app|
+      envs = get_environments(app.id)
+      envs.each do |env|
+        begin
+          latest_deploy = get_latest_deploy(app.id, env.id)
+        rescue GRPC::NotFound
+          latest_deploy = nil
+        end
+        if latest_deploy&.state == "success"
+          @num_deployments += 1
+        end
+      end
+    end
+  end
+
+  private
+
+  def get_environments(app_id)
+    req = Soapbox::ListEnvironmentRequest.new(application_id: app_id)
+    $api_environment_client.list_environments(req).environments
+  end
+
+  def get_latest_deploy(app_id, env_id)
+    req = Soapbox::GetLatestDeploymentRequest.new(application_id: app_id, environment_id: env_id)
+    $api_deployment_client.get_latest_deployment(req)
   end
 end

dea5b47a976ba0558df933648bfa7e714dd6045c
Chris Johnson <chris.johnson@adhocteam.us>
2017-11-28 16:14:17 -0800

    Fixes user/repo name parsing from github URL.

--- web/app/controllers/deployments_controller.rb
+++ web/app/controllers/deployments_controller.rb
@@ -73,7 +73,7 @@ class DeploymentsController < ApplicationController
   end
 
   def find_commits
-    github_repo = @app.github_repo_url.gsub(%r{https://.*@github.com/}, '').gsub(/\.git$/, '')
+    github_repo = @app.github_repo_url.split('github.com/').last.gsub(/\.git$/, '')
     @commits = octokit.commits(github_repo, 'master', per_page: 1000).map do |c|
       ["#{c.commit.message} (#{c.sha})", c.sha]
     end
